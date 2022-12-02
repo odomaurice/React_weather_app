d1773129ef5ea3a45aa07037eb0fbaa46f297526
Odo Maurice{alphaCodes} <92038525+odomaurice@users.noreply.github.com>
2022-12-02 20:42:03 +0100

    font and background update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Search from "./components/search/search";
 import Forecast from "./components/forecast/forecast";
 import CurrentWeather from "./components/current-weather/current-weather";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import "./App.css";
 
-
-function App() {
-
+function App(data) {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
   const handleOnSearchChange = (searchData) => {
@@ -23,20 +21,20 @@ function App() {
 
     Promise.all([currentWeatherFetch, forecastWeatherFetch])
       .then(async (response) => {
-       const weatherResponse = await response[0].json();
-       const forecastResponse = await response[1].json();
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
 
-       setCurrentWeather({city: searchData.label, ...weatherResponse});
-       setForecast({city: searchData.label, ...forecastResponse});
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
       })
       .catch((err) => console.log(err));
   };
 
-    console.log(currentWeather);
-    console.log(forecast);
+  console.log(currentWeather);
+  console.log(forecast);
 
   return (
-    <div className="container">
+    <div className={typeof data.main !== "undefined" ? data.main.temp > 16 ? "App warm" : "App" : "App"}>
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
       {forecast && <Forecast data={forecast} />}
--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -22,7 +22,7 @@ const Forecast = ({data}) => {
       <>
         <label className="title">Daily Forecast</label>
         <Accordion allowZeroExpanded>
-          {data.list.splice(0, 7).map((item, idx) => (
+          {data.list.slice(0, 7).map((item, idx) => (
             <AccordionItem key={idx}>
               <AccordionItemHeading>
                 <AccordionItemButton>
