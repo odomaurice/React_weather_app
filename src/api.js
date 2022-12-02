38272d03d783df42c043e51e044900bca9970aa5
Odo Maurice{alphaCodes} <92038525+odomaurice@users.noreply.github.com>
2022-12-02 20:31:27 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "0ff36d03acmsh77883120d63a319p181d0cjsn85550e131581",
+    "X-RapidAPI-Key": "",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
     
   },
@@ -11,4 +11,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY =  "2eab6e56f3258f1e0e737f21d055e560";
+export const WEATHER_API_KEY =  "";
